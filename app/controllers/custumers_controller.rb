8a82d6713b82398454a917a87fa8930a70813c23
Matheus <matheus__e@hotmail.com>
2016-05-09 14:44:46 -0300

    Add paginação para costomers e alterado layout

--- app/controllers/custumers_controller.rb
+++ app/controllers/custumers_controller.rb
@@ -1,11 +1,17 @@
 class CustumersController < ApplicationController
+  PAGE_SIZE = 10
+
   def index
+    @page = (params[:page] || 0).to_i
+
     if params[:keywords].present?
       @keywords = params[:keywords]
       custumer_search_term = CustumerSearchTerm.new(@keywords)
       @custumers = Custumer.where(
         custumer_search_term.where_clause,
-        custumer_search_term.where_args).order(custumer_search_term.order)
+        custumer_search_term.where_args)
+        .order(custumer_search_term.order).
+        offset(PAGE_SIZE * @page).limit(PAGE_SIZE)
     else
       @custumers = []
     end
